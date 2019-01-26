81ac24c2c0c2a4ed76e6477cab72a1e6fc29f198
Ledodo99 <luis.edo.1999@gmail.com>
2019-01-26 13:59:34 -0600

    Torreta terminada

---
+++ Assets/Scripts/AttackTorreta.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackTorreta : MonoBehaviour
+{
+	public GameObject objetivoActual = null;
+	public GameObject bala;
+	public float reloadTime = 1f;
+	public float currentReload = 0f;
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+	{
+		if (objetivoActual != null)
+		{
+		transform.LookAt(objetivoActual.transform);
+		if (currentReload < reloadTime)
+		{
+			currentReload += Time.deltaTime;
+		}
+		else
+		{
+			currentReload = 0f;
+			Instantiate(bala,new Vector3(0,0,0),new Quaternion(0,0,0,0),transform);
+		}
+		}  
+	}
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Enemigo")
+		{
+			if (objetivoActual == null)
+			{
+				objetivoActual = other.gameObject;
+			}
+		}
+
+	}
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject == objetivoActual)
+			objetivoActual = null;
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "Enemigo")
+		{
+			if (objetivoActual == null)
+			{
+				objetivoActual = other.gameObject;
+			}
+			else
+			{
+				float distanciaOA = Vector3.Distance(objetivoActual.transform.position, transform.position);
+				float distanciaNO = Vector3.Distance(other.gameObject.transform.position, transform.position);
+				if (distanciaNO < distanciaOA)
+				{
+					objetivoActual = other.gameObject;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/bulletFlight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletFlight : MonoBehaviour
+{
+	public GameObject objetivoActual;
+	public float velocidad = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.Translate(Vector3.forward*velocidad*Time.deltaTime);
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Enemigo" || other.gameObject.tag=="Pared")
+		{
+			Destroy(gameObject);
+		}
+	}
+}

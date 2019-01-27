582c620a7449a3ce724cf8b74127b9b0bf6da3d5
Ledodo99 <luis.edo.1999@gmail.com>
2019-01-26 19:33:56 -0600

    Trampa De Osos Terminada

---
+++ Assets/Scripts/trapDoor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trapDoor : MonoBehaviour
+{
+	public float tiempoDeTrampa = 4f;
+	Collider activador;
+    // Start is called before the first frame update
+    void Start()
+    {
+		activador = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (activador.enabled == false)
+		{
+			tiempoDeTrampa -= Time.deltaTime;
+		}
+		if (tiempoDeTrampa <= 0)
+		{
+			Destroy(gameObject);
+		}
+    }
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Enemigo")
+		{
+			activador.enabled = false;
+
+		}
+	}
+}

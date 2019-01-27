1a3452f5cdbe88cad78ba299afc0ccbe64608f74
Ledodo99 <luis.edo.1999@gmail.com>
2019-01-27 14:17:33 -0600

    Panico Del Pato

--- Assets/Scripts/Perro.cs
+++ Assets/Scripts/Perro.cs
@@ -1,21 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Perro : MonoBehaviour
 {
+	public Animator anim;
 	public GameObject cilindro;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		anim = GetComponent<Animator>();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
 
 	private void OnTriggerEnter(Collider other)
 	{
@@ -26,5 +26,13 @@ public class Perro : MonoBehaviour
 	{
 		if (other.gameObject.tag == "Enemigo")
 			cilindro.SendMessage("attack");
+	}
+	public void EstaAtacando()
+	{
+		anim.SetBool("isAttacking", true);
+	}
+	public void EstaParado()
+	{
+		anim.SetBool("isAttacking", false);
 	}
 }
